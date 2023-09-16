8ab3f02e8b910a396d947b4c338c599be0f87519
strongpapazola <strongpapazola@gmail.com>
2023-09-16 09:48:11 +0700

    init

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'crud_db';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
